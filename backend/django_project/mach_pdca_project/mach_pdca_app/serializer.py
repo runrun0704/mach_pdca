20c574f2643f87a819349452188e4bd066148881
runrun0704 <satoru0704.f@gmail.com>
2022-07-17 14:53:25 +0900

    22/7/17 restframeworkで簡易的APIを構築した

--- backend/django_project/mach_pdca_project/mach_pdca_app/serializer.py
+++ backend/django_project/mach_pdca_project/mach_pdca_app/serializer.py
@@ -1,1 +1,12 @@
-li
+from rest_framework import serializers
+from .models import PdcaModel,User,Condition,Tag
+
+class PdcaModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PdcaModel
+        fields = ('p','d','c','a')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name')    
--- backend/django_project/mach_pdca_project/mach_pdca_project/urls.py
+++ backend/django_project/mach_pdca_project/mach_pdca_project/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from mach_pdca_app.urls import router as app_api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('mach_pdca_app.urls'))
+    path('api/', include(app_api_router.urls)),
+    path('',include('mach_pdca_app.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
