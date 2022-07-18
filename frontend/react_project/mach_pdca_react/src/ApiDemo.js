b314bfb170afb7152853706e73d842b34e742699
runrun0704 <satoru0704.f@gmail.com>
2022-07-18 17:28:53 +0900

    22/7/18 apidemo.jsを追加。試験用のファイル

---
+++ frontend/react_project/mach_pdca_react/src/ApiDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ApiDemo extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>APIのdemoを表示したい</h2>
+            </div>
+        )
+    }
+}
+export default ApiDemo;
--- frontend/react_project/mach_pdca_react/src/App.js
+++ frontend/react_project/mach_pdca_react/src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ApiDemo from './ApiDemo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2 className = 'text'>text</h2>
-      </header>
+      <ApiDemo />
     </div>
   );
 }
