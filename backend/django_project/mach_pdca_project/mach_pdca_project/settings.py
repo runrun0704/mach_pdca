9e97daab0f0cc95bcf3068fee40f0dfd273dbe97
runrun0704 <satoru0704.f@gmail.com>
2022-07-26 17:02:05 +0900

    22/7/26 django update to 4.0,python update to 3.10.5

--- backend/django_project/mach_pdca_project/mach_pdca_project/settings.py
+++ backend/django_project/mach_pdca_project/mach_pdca_project/settings.py
@@ -43,14 +43,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'mach_pdca_project.urls'
@@ -72,10 +72,12 @@ TEMPLATES = [
 ]
 
 #'CORS_ALLOWED_WHITELIST'の可能性有り
-CORS_ALLOWED_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 WSGI_APPLICATION = 'mach_pdca_project.wsgi.application'
 
