a1d8b602c25b8e63cecfdd4e96cdde58db23c1af
runrun0704 <satoru0704.f@gmail.com>
2022-07-31 15:06:35 +0900

    22/7/31 APIのルーティングをapis.pyにまとめた

--- backend/django_project/mach_pdca_project/mach_pdca_project/urls.py
+++ backend/django_project/mach_pdca_project/mach_pdca_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from mach_pdca_app.urls import router as app_api_router
+from mach_pdca_app.apis import router as app_api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
