8dfaae856c9b6d99b26fb11110e42c6f3e7f17f0
runrun0704 <satoru0704.f@gmail.com>
2022-09-02 16:14:07 +0900

    avoid error for not found

--- frontend/react_project/mach_pdca_react/src/App.js
+++ frontend/react_project/mach_pdca_react/src/App.js
@@ -4,6 +4,7 @@ import ApiGet from './Api_get'
 import ApiPost from './Api_post'
 import ApiPut from './Api _put'
 import ApiDelete from './Api＿delete'
+import NotFound from './NotFound'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route  exact path="/" element={<ApiGet />}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
