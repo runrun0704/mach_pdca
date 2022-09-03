01cdbb38572365cbd5eaf163f64fbb7f9351738a
runrun0704 <satoru0704.f@gmail.com>
2022-09-03 17:13:07 +0900

    ApiPutis added Navigate

--- frontend/react_project/mach_pdca_react/src/Api_detail.js
+++ frontend/react_project/mach_pdca_react/src/Api_detail.js
@@ -17,22 +17,26 @@ function ApiDetail(){
     return (
         <div>
             <div>
-                    <p>{pdca.id}</p>
-                    <p>{pdca.p}</p>
-                    <p>{pdca.d}</p>
-                    <p>{pdca.c}</p>
-                    <p>{pdca.a}</p>
-                    <p>{pdca.title}</p>
-                    <p>{pdca.goal}</p>
-                    <p>{pdca.weight}</p>
-                    <p>{pdca.comment}</p>
-                    <p>{pdca.condition}</p>
-                    <p>{pdca.tag_name}</p>
-                    <p>{pdca.user_name}</p>
-                    <p>{pdca.created}</p>
-                    <p>{pdca.updated}</p>
-                    <NavLink to={'/'}>戻る</NavLink>
-                </div>
+                <p>{pdca.id}</p>
+                <p>{pdca.p}</p>
+                <p>{pdca.d}</p>
+                <p>{pdca.c}</p>
+                <p>{pdca.a}</p>
+                <p>{pdca.title}</p>
+                <p>{pdca.goal}</p>
+                <p>{pdca.weight}</p>
+                <p>{pdca.comment}</p>
+                <p>{pdca.condition}</p>
+                <p>{pdca.tag_name}</p>
+                <p>{pdca.user_name}</p>
+                <p>{pdca.created}</p>
+                <p>{pdca.updated}</p>
+                
+            </div>
+            <div>
+                <NavLink to={`/pdca/${id}/update`}>更新</NavLink>
+                <NavLink to={'/'}>戻る</NavLink>
+            </div>
         </div>
     );
 }
